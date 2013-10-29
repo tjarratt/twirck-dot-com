46d3b875bd5e732a81962a48450b2b7cd5eeb9cc
Tim Jarratt <tjarratt@gmail.com>
2013-10-28 22:35:21 -0700

    Redesign homepage
    Added links to app store. Unhappy with how bootstrap has ended up making
    the alignment of everything SO SCREWY, but maybe that's because I have
    no idea how bootstrap works.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TwirckWebsite::Application.routes.draw do
   root :to => 'splash#index'
+
   match 'privacy' => 'splash#privacy'
+  match 'about' => 'splash#about'
+
   post 'feedback' => 'feedback#create'
 end
